2ff8819852439be05cdb6654b2b8faf5c39f0d7a
danishsshaikh <danishsshaikh@gmail.com>
2020-03-16 15:57:51 +0530

    :four:

--- chatbot_database.py
+++ chatbot_database.py
@@ -2,6 +2,9 @@ import sqlite3
 import json
 from datetime import datetime
 
+#D:\IDM
+#C:\Users\Acer\Desktop\chatbot
+
 timeframe = '2015-05'
 sql_transaction =[]
 
@@ -9,31 +12,33 @@ connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
 
 def create_table():
-    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply
-(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT,
-comment TEXT, subreddit TEXT, unix INT, score INT)""")
+    c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
 
 def format_data(data):
     data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
     return data
 
-if __name__ == '__main__':
-    create_table()
-    row_counter = 0
-    paired_rows = 0
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
+        return False    
 
-    with open('C:\Users\Acer\Desktop\chatbot/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
-        for row in f:
-            row_counter += 1
-            row = json.loads(row)
-            parent_id = row['parent_id']
-            body = format_data(row['body'])
-            created_utc = row['created_utc']
-            score = row['score']
-            comment_id = row['name']
-            subreddit = row['subreddit']
-
-            parent_data = find_parent(parent_id)
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data = '[deleted]' or data = '[removed]':
+        return False
+    else:
+        return True
 
 def find_parent(pid):
     try:
@@ -46,3 +51,29 @@ def find_parent(pid):
     except Exception as e:
         #print(str(e))
         return False
+
+if __name__ == '__main__':
+    create_table()
+    row_counter = 0
+    paired_rows = 0
+
+    with open('D:/IDM/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
+        for row in f:
+            #print(row)
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            comment_id = row['name']
+            subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+
+            if score >= 5:
+                existing_comment_score = find_existing_score(parent_id)
+                if existing_comment_score:
+                    if score > existing_comment_score:
+                        
+
+
